d0afedaaa8c2374275f10885a53c217607dda139
silveryferret <silveryferret@gmail.com>
2016-12-23 18:52:33 -0600

    Attempts to add outgoing packet handling

--- earlybot.py
+++ earlybot.py
@@ -1,9 +1,11 @@
 import discord
 import asyncio
+import struct
 import ast
 
 host = "localhost"
 port = 4213
+gameport = 61926
 token = "MjYxNDI2NDM1OTcxODc0ODE2.Cz4GvQ.nEJwFbd61MzZ_HXXldhAJgOyeiE"
 ahelpID = "260863607661658112"
 mainID = "260863628582977547"
@@ -20,9 +22,34 @@ class Aphrodite(discord.Client):
         if message.content.startswith("!q"):
             yield from self.send_message(message.channel, "See ya, %s!" % author)
             yield from self.logout()
-            yield from loop.stop()
+            loop.stop()
+            
+        elif message.content.startswith("!status"):
+            yield from handle_outgoing("status", loop)
             
 ourBot = Aphrodite()
+
+def formatpacket(msg):
+    return b"\x00\x83" + struct.pack(">H", len(msg) + 6) + \
+    b"\x00\x00\x00\x00\x00" + bytes(msg, "ascii") + b"\x00"
+    
+@asyncio.coroutine
+def handle_outgoing(payload, loop):
+
+    print("Entering handle_outgoing")
+    reader, writer = yield from asyncio.open_connection(host, gameport, loop=loop)
+    print("Connection opened.")    
+    packet = formatpacket(payload)
+    print("Packet formatted.")    
+    writer.write(bytes(payload, "ascii"))
+    print("Packet sent.")
+    received = yield from reader.read(-1)
+    print("Packet received.")
+    print(received)
+    print("Closing socket.")
+    writer.close()
+    print("Socket closed.")
+    
 
 @asyncio.coroutine
 def handle_incoming(reader, writer):
@@ -44,7 +71,10 @@ def handle_queue():
     
     queuedMsg = yield from queue.get()
     loop.create_task(handle_queue())
-    print(queuedMsg)
+    if queuedMsg.startswith("Request for Help") or queuedMsg.startswith("Reply"):
+        yield from ourBot.send_message(ourBot.get_channel(ahelpID), queuedMsg)
+    else:
+        yield from ourBot.send_message(ourBot.get_channel(mainID), queuedMsg)
     
 def main():
  
