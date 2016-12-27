14802dcbb57c87a86b160d97a9194292eb9f2be3
silveryferret <silveryferret@gmail.com>
2016-12-27 10:19:23 -0600

    Fixes, formatting changes

--- BotCommands.py
+++ BotCommands.py
@@ -122,7 +122,7 @@ class Manifest(Command):
             manifest = ast.literal_eval(manifest)
             manifestMsg = "```"
             if manifest == []:
-                manifestMsg = "No crew found."
+                manifestMsg += "No crew found."
             else:    
                 try:
                     manifestMsg = self.fill_departments(manifest, manifest["heads"], "Command:\r\n", manifestMsg)
@@ -259,26 +259,37 @@ class AdminMsg(Command):
 class Notes(Command):
 
     def parse(self, qs):
-        qs = qs.strip("+")
-        qs = qs.replace("+", " ")
+        qs = qs.replace("%26%2334%3b", '"')
+        qs = qs.replace("%26%2339%3b", '"')
+        qs = qs.replace("%26amp%3b", "&")
+        qs = qs.replace("%26%2339;", "'")
         qs = qs.replace("%0d", "\r")
         qs = qs.replace("%0a", "\n")
         qs = qs.replace("%28", "(")
         qs = qs.replace("%29", ")")
+        qs = qs.replace("%2b", "+")
         qs = qs.replace("%2c", ",")
+        qs = qs.replace("%2f", "/")
+        qs = qs.replace("%3a", ":")
+        qs = qs.replace("%3b", ";")
+        qs = qs.replace("%3f", "?")
+        qs = qs.replace("%5b", "[")
+        qs = qs.replace("%5d", "]")
+        qs = qs.replace("+", " ")
         return qs
         
     def format_for_sending(self, message):
         message = "```" + message + "```"
         return message
         
+    @asyncio.coroutine
     def send(self, qs):
         message = self.parse(qs)
-        if len(message) > 1994:
-            fmtmessage = self.format_for_sending(message[1994:])
+        if len(message) >= 1994:
+            fmtmessage = self.format_for_sending(message[:1994])
             yield from self.client.send_message(self.message.channel, fmtmessage)
             message = message[1994:]
-            self.send(message)
+            yield from self.send(message)
         else:
             message = "```" + message + "```"
             yield from self.client.send_message(self.message.channel, message)
@@ -290,7 +301,7 @@ class Notes(Command):
             command = "?notes=" + commandtup[1]
             command += ";key=" + config.commskey
             qs = yield from handle_outgoing(command, self.loop)
-            send(qs)
+            yield from self.send(qs)
         except OSError:
             yield from self.client.send_message(self.message.channel, "Server is offline.")
 
@@ -303,7 +314,10 @@ class Age(Command):
             command = "?age=" + commandtup[1]
             command += ";key=" + config.commskey
             age = yield from handle_outgoing(command, self.loop)
-            print(age)
+            if age == "Ckey not found":
+                yield from self.client.send_message(self.message.channel, age)
+            else:
+                yield from self.client.send_message(self.message.channel, "Account is %s days old." % age)
         except OSError:
             yield from self.client.send_message(self.message.channel, "Server is offline.")
 
@@ -317,7 +331,8 @@ class Help(Command):
         helpMsg = ""
         helpMsg += "```Aphrodite Bot Commands:\r\n"
         helpMsg += prepend + "ping                 - checks if server is up\r\n"
-        helpMsg += prepend + "status               - status, including round duration, station time, players online\r\n"
+        helpMsg += prepend + "status               - status, including round duration, station time,\r\n"
+        helpMsg += "\tplayers online\r\n"
         helpMsg += prepend + "manifest             - shows in round crew manifest\r\n"
         if self.message.channel.id == config.ahelpID:
             helpMsg += prepend + "info <ckey>          - shows detailed information about ckey\r\n"
